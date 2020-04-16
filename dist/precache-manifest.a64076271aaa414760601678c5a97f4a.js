29c3bc72baf9717207aafe37ec58f39fedd79b4d
Abdelrhman Tareq <abdonebny22@gmail.com>
2020-04-16 11:03:06 +0200

    first comment

---
+++ dist/precache-manifest.a64076271aaa414760601678c5a97f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fe24c6c042242f9545e01f6bcadc64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "764c25364ea20440cb27",
+    "url": "/static/css/main.1869182d.chunk.css"
+  },
+  {
+    "revision": "422dfa27f44c6e1510d8",
+    "url": "/static/js/2.c3d817b0.chunk.js"
+  },
+  {
+    "revision": "a7859b41c23bed46d2a2cd4146c86afb",
+    "url": "/static/js/2.c3d817b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764c25364ea20440cb27",
+    "url": "/static/js/main.587a93ba.chunk.js"
+  },
+  {
+    "revision": "11f7798a071b8874132f",
+    "url": "/static/js/runtime-main.7d83fb96.js"
+  }
+]);
